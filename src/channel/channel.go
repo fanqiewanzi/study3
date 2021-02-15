e0f7fedf0c5288ca5dc5d60b4fdc32ebcad8a190
BAZINGA <2406357659@qq.com>
2021-02-15 15:22:15 +0800

    update

--- src/channel/channel.go
+++ src/channel/channel.go
@@ -24,12 +24,15 @@ func print2(ch chan int) {
 }
 func PrintTest() {
 	//创建一个能容纳100个int类型的管道
-	chs := make(chan int, 100)
+	chs := make(chan int, 1)
 	for i := 1; i <= 100; i++ {
-		//给管道赋值
 		chs <- i
 		//交给goroutine进行打印输出
 		go print1(chs)
+		i++
+		//给管道赋值
+		chs <- i
+		//交给goroutine进行打印输出
 		go print2(chs)
 	}
 	//等待两秒等所有goroutine运行完
